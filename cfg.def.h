f58ac92dfeb74d04301bb079013a3fd996574010
William Casarin <jb55@jb55.com>
2018-07-09 21:47:36 -0700

    BIGGER

--- cfg.def.h
+++ cfg.def.h
@@ -18,7 +18,7 @@ static const char *separator = " → ";
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=15"
+	"monospace:size=16"
 };
 
 #define scheme_bg "#222222"
