a896a4ae77fd67df386f0eacd3ae520dd4e88f5b
William Casarin <jb55@jb55.com>
2019-01-23 14:52:05 -0800

    email-fetch

--- cfg.def.h
+++ cfg.def.h
@@ -79,7 +79,7 @@ static struct command email_notifications[] = {
 };
 
 static struct command email_commands[] = {
-	DEFCMD("f", "fetch", "systemctl --user restart email-fetcher")
+	DEFCMD("f", "fetch", "email-fetch")
 	DEFCMD("m", "sync monad", "n muchsync monad-local")
 	DEFCMD("s", "status", "n email-status-once")
 	DEFPREFIX("n", "notifications", email_notifications)
